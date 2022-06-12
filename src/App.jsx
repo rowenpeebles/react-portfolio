8c8635756dbaf74098d35585e4d37ce229e48e36
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-13 00:16:24 +0100

    Adding nav component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Nav from './components/Nav/Nav'
 import About from './components/About/About'
-import Experience from './components/Experience/Experience'
-import Services from './components/Services/Services'
 import Portfolio from './components/Portfolio/Portfolio'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
@@ -14,8 +12,6 @@ const App = () => {
         <Header />
         <Nav />
         <About />
-        <Experience />
-        <Services />
         <Portfolio />
         <Contact />
         <Footer />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
   return (
     <header>
         <div className="container header__container">
-            <h5>Hello I'm</h5>
+            <h5>Hey I'm</h5>
             <h1>Rowen Peebles</h1>
-            <h5 className="text-light">Fullstack Developer</h5>
+            <h5 className="text-light">A Fullstack Developer</h5>
             <CTA />
             <HeaderSocials />
 
