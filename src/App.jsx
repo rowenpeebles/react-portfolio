a33841f09ea4c8a1f1eab0156f94f28356cc13b2
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-12 20:08:36 +0100

    Adding project structure and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Experience from './components/Experience/Experience'
+import Services from './components/Services/Services'
+import Portfolio from './components/Portfolio/Portfolio'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Contact />
+        <Footer />
+    </>
   )
 }
 
