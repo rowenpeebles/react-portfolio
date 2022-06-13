87899dc9b30ee7365c238c01780bb263d398cd4f
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-13 15:31:03 +0100

    Adding Experience component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Nav from './components/Nav/Nav'
 import About from './components/About/About'
+import Experience from './components/Experience/Experience'
 import Portfolio from './components/Portfolio/Portfolio'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
@@ -12,6 +13,7 @@ const App = () => {
         <Header />
         <Nav />
         <About />
+        <Experience />
         <Portfolio />
         <Contact />
         <Footer />
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
-import { AiOutlineUser } from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
-import {BiMessageSquareDetail } from 'react-icons/bi'
-import { useState } from 'react'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
 
@@ -19,6 +20,10 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')}
        className={activeNav === '#about' ? 'active' : ''}>
         <AiOutlineUser/></a>
+
+        <a href="#experience" onClick={() => setActiveNav('#experience')} 
+      className={activeNav === '#experience' ? 'active' : ''}>
+        <RiServiceLine/></a>
 
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')}
        className={activeNav === '#portfolio' ? 'active' : ''}>
